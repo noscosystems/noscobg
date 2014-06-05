23837557d26c743238e7aa7e0635f20496540dd7
Ivaylo Raykov <ivailo_raikov@hotmail.com>
2014-06-05 13:19:19 +0300

    Create a foren keys

--- application/migrations/m140605_095258_create_foreign_keys_1.php
+++ application/migrations/m140605_095258_create_foreign_keys_1.php
@@ -6,6 +6,11 @@ class m140605_095258_create_foreign_keys_1 extends CDbMigration
 	{
 		// $this->addForeignKey('name_of_key', '{{table_from}}', 'column_from', '{{table_to}}', 'column_to');
 		// $this->addForeignKey('fk_assets_address_id', '{{assets}}', 'address', '{{address}}', 'id');
+		$this->addForeignKey('fk_users_title_option', '{{users}}', 'title', '{{option}}', 'id')
+		$this->addForeignKey('fk_assets_type_option', '{{assets}}', 'type', '{{option}}', 'id')
+		$this->addForeignKey('fk_assets_created_by_user', '{{assets}}', 'created_by', '{{user}}', 'id')
+		$this->addForeignKey('fk_assets_address_address', '{{assets}}', 'address', '{{address}}', 'id')
+		$this->addForeignKey('fk_images_asset_asset', '{{images}}', 'asset', '{{asset}}', 'id')
 	}
 
 	public function down()
