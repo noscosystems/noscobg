f578e2b0b25253671629f0077fc8520f935696f4
pavelpravchev <p_pravchev@abv.bg>
2014-06-05 13:33:20 +0300

    fixed some mistakes in table/field names

--- application/migrations/m140605_095258_create_foreign_keys_1.php
+++ application/migrations/m140605_095258_create_foreign_keys_1.php
@@ -9,14 +9,14 @@ class m140605_095258_create_foreign_keys_1 extends CDbMigration
 
 		$this->addForeignKey('fk_users_title_option', '{{users}}', 'title', '{{option}}', 'id');
 		$this->addForeignKey('fk_assets_type_option', '{{assets}}', 'type', '{{option}}', 'id');
-		$this->addForeignKey('fk_assets_created_by_user', '{{assets}}', 'created_by', '{{user}}', 'id');
+		$this->addForeignKey('fk_assets_created_by_user', '{{assets}}', 'created_by', '{{users}}', 'id');
 		$this->addForeignKey('fk_assets_address_address', '{{assets}}', 'address', '{{address}}', 'id');
-		$this->addForeignKey('fk_images_asset_asset', '{{images}}', 'asset', '{{asset}}', 'id');
-		$this->addForeignKey('fk_owners_asset_asset', '{{owners}}', 'asset', '{{asset}}', 'id');
-		$this->addForeignKey('fk_owners_user_user', '{{owners}}', 'user', '{{user}}', 'id');
+		$this->addForeignKey('fk_images_asset_assets', '{{images}}', 'asset', '{{assets}}', 'id');
+		$this->addForeignKey('fk_owners_asset_assets', '{{owners}}', 'asset', '{{assets}}', 'id');
+		$this->addForeignKey('fk_owners_user_users', '{{owners}}', 'user', '{{users}}', 'id');
 		$this->addForeignKey('fk_rooms_type_type', '{{rooms}}', 'type', '{{option}}', 'id');
-		$this->addForeignKey('fk_rooms_asset_asset', '{{rooms}}', 'asset', '{{asset}}', 'id');
-		$this->addForeignKey('fk_rooms_created_by_created_by', '{{rooms}}', 'created_by', '{{user}}', 'id');
+		$this->addForeignKey('fk_rooms_asset_asset', '{{rooms}}', 'asset', '{{assets}}', 'id');
+		$this->addForeignKey('fk_rooms_created_by_created_by', '{{rooms}}', 'created_by', '{{users}}', 'id');
 
 	}
 
